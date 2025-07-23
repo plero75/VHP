bc1daea53a20b45dc3bdfa61aa3445557b33561a
plop14 <70171493+plero75@users.noreply.github.com>
2025-07-24 00:21:46 +0200

    Add files via upload

--- modules/navitia.js
+++ modules/navitia.js
@@ -5,25 +5,20 @@ const stopMonitoringUrl = `${proxy}/?url=https://prim.iledefrance-mobilites.fr/m
 
 export async function getNextStops() {
   try {
-    const resp1 = await fetch(stopMonitoringUrl);
-    const data1 = await resp1.json();
+    const resp = await fetch(stopMonitoringUrl);
+    const data = await resp.json();
 
-    if (!data1?.Siri?.ServiceDelivery?.StopMonitoringDelivery?.length) {
-      throw new Error("Pas de données retournées depuis stop-monitoring");
-    }
+    const visits = data?.Siri?.ServiceDelivery?.StopMonitoringDelivery?.[0]?.MonitoredStopVisit;
+    if (!visits || visits.length === 0) throw new Error("Aucun train trouvé");
 
-    const visit = data1.Siri.ServiceDelivery.StopMonitoringDelivery[0].MonitoredStopVisit?.[0];
-    const vjId = visit?.MonitoredVehicleJourney?.FramedVehicleJourneyRef?.DatedVehicleJourneyRef;
-
-    if (!vjId) {
-      throw new Error("Aucun train détecté ou pas d'ID de trajet");
-    }
-
-    const encId = encodeURIComponent(vjId);
-    const resp2 = await fetch(`${proxy}/?url=https://prim.iledefrance-mobilites.fr/marketplace/v2/navitia/vehicle_journeys/${encId}/stop_times`);
-    const data2 = await resp2.json();
-    return data2.stop_times || [];
-  } catch(err) {
+    const monitoredCalls = visits[0]?.MonitoredVehicleJourney?.OnwardCalls?.OnwardCall || [];
+    return monitoredCalls.map(call => ({
+      name: call.StopPointName?.value || "???",
+      expected: call.ExpectedArrivalTime,
+      aimed: call.AimedArrivalTime,
+      destination: visits[0]?.MonitoredVehicleJourney?.DestinationName?.value || ""
+    }));
+  } catch (err) {
     console.error("Erreur dans getNextStops :", err);
     return [];
   }
